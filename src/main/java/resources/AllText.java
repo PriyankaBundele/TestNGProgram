092ccb1dcd66f7fa384a0d0a29f5aa3204e3657c
priyanka <priyankabundele@gmail.com>
2023-01-02 19:16:53 +0530

    added project

--- src/main/java/resources/AllText.java
+++ src/main/java/resources/AllText.java
@@ -6,7 +6,7 @@ public class AllText {
 	public static String password3= "123";
 
 	public static String E = "priyanka";
-	public static String TextAppeard = "Please check your username password. If you still can't log in, contact your Salesforce administrator";
+	public static String TextAppeard = "Please check your username and password. If you still can't log in, contact your Salesforce administrator";
 	public static String Firstname2 = "priyanka";
 	public static String LastNm = "machle";
 	public static String phone="7875718550";
