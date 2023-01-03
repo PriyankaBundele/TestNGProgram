ceba84e136cbd9925c38569f96e564ef8cdebc2c
priyanka <priyankabundelel@gmail.com>
2023-01-03 17:54:34 +0530

    added changes in alltext

--- src/main/java/resources/AllText.java
+++ src/main/java/resources/AllText.java
@@ -1,17 +1,17 @@
 package resources;
 
 public class AllText {
-	public static String username5 = "priyanka";
+	public static String username5 = "swiss";
 
 	public static String password3= "123";
 
 	public static String E = "priyanka";
 	public static String TextAppeard = "Please check your username and password. If you still can't log in, contact your Salesforce administrator";
-	public static String Firstname2 = "priyanka";
-	public static String LastNm = "machle";
-	public static String phone="7875718550";
+	public static String Firstname2 = "swiss";
+	public static String LastNm = "lobert";
+	public static String phone="8895718530";
 	public static String jobEntitle = "Associate";
-	public static String rightemail  = "priyankabundelel@gmail.com";
+	public static String rightemail  = "swissworld@gmail.com";
 	public static String Company  = "Accenture";
 
 
